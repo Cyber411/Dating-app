9eeb3cbb77a0c9a607de2fe8c71b207a08c34b7a
Ben <omongoleb@gmail.com>
2025-01-07 15:35:40 +0300

    database datails update

--- dating/settings.py
+++ dating/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'dating.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dating',
-        'USER':'postgres',
-        'PASSWORD':'pCyberDBDataBase',
-        #'HOST':'localhost',
-        #'PORT':'5432'
+        'NAME': 'daters',
+        'USER':'daters_user',
+        'PASSWORD':'OGg4aJ82jRhrI0DSKHanU1VWj3XUPW6e',
+        'HOST':'dpg-ctsif4tumphs73fmsq90-a',
+        'PORT':'5432'
         
     }
 }
