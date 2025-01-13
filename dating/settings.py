ed3f27c6ea99b53d66743a7212f50923be75da94
Ben <omongoleb@gmail.com>
2025-01-13 15:29:51 +0300

    database

--- dating/settings.py
+++ dating/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 }
 #postgresql://daters_user:OGg4aJ82jRhrI0DSKHanU1VWj3XUPW6e@dpg-ctsif4tumphs73fmsq90-a.oregon-postgres.render.com/daters
 
-DATABASES['default']=  dj_database_url.parse("postgresql://daters_user:OGg4aJ82jRhrI0DSKHanU1VWj3XUPW6e@dpg-ctsif4tumphs73fmsq90-a.oregon-postgres.render.com/daters")
+DATABASES['default']=  dj_database_url.parse("postgresql://daters_user:OGg4aJ82jRhrI0DSKHanU1VWj3XUPW6e@dpg-ctsif4tumphs73fmsq90-a/daters")
  
 
 # Password validation
